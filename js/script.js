d0b2af18f4eec5ba6ffcf206fba2054f62fc587a
Shini084 <79849892+shini084@users.noreply.github.com>
2023-02-25 17:23:56 -0300

    animation

--- js/script.js
+++ js/script.js
@@ -1,12 +1,17 @@
 function lucario(){
     var screen = document.querySelector('#screenId')
 
-    for(r=0;r<64;r++){
-        for(c=0;c<64;c++){
-            var pixel = document.createElement('div')
+    for(r=0;r<66;r++){
+        for(c=0;c<53;c++){
+            let pixel = document.createElement('div')
             pixel.className = 'pixel'
             pixel.id = `c${c}r${r}`
+            
+            pixel.style.animation = `sand 2s  ease ${
+               (((r - 65)%2 * -1)*64 + ((c - 64) * -1)) / 20
+            }s 1 normal forwards`
             screen.appendChild(pixel)
         }
     }
 }
+
